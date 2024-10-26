82e58139b58f24b72e5998d8f5ad352f81bf0583
Tony Lam <tony.lam@iki.fi>
2024-10-26 12:17:04 +0300

    Feat: user continuation

--- main.py
+++ main.py
@@ -6,20 +6,25 @@ print(
 1-5 asteikkoon ja lineaariseen pisteytysmalliin"
 )
 
-maksimipisteet = float(input("Anna pistemäärä arvosanalle 5: "))
-minimipisteet = float(input("Anna pistemäärä arvosaanalle 1: "))
+while True:
+    maksimipisteet = float(input("Anna pistemäärä arvosanalle 5: "))
+    minimipisteet = float(input("Anna pistemäärä arvosaanalle 1: "))
+    kayttajapisteet = float(input("Anna saamasi pistemäärä: "))
 
-kayttajapisteet = float(input("Anna saamasi pistemäärä: "))
+    # if user points are less than minimum points, user gets 0 grade
+    if kayttajapisteet < minimipisteet:
+        print("Arvosanasi on 0, et päässyt läpi.")
+    # if user points are larger than maximum points, user gets 5 grade
+    elif kayttajapisteet >= maksimipisteet:
+        print("Arvosanasi on 5")
+    # if user points are smaller than maximum points linear calculation
+    elif kayttajapisteet < maksimipisteet:
+        arvosana = 1 + 4 * (kayttajapisteet - minimipisteet) / (
+            maksimipisteet - minimipisteet
+        )
+        print("Arvosanasi on", math.floor(arvosana))
 
-# if user points are less than minimum points, user gets 0 grade
-if kayttajapisteet < minimipisteet:
-    print("Arvosanasi on 0, et päässyt läpi.")
-# if user points are larger than maximum points, user gets 5 grade
-elif kayttajapisteet >= maksimipisteet:
-    print("Arvosanasi on 5")
-# if user points are smaller than maximum points linear calculation
-elif kayttajapisteet < maksimipisteet:
-    arvosana = 1 + 4 * (kayttajapisteet - minimipisteet) / (
-        maksimipisteet - minimipisteet
-    )
-    print("Arvosanasi on", math.floor(arvosana))
+    # ask user if they want to continue
+    userContinues = input("Haluatko jatkaa? (Y/N): ")
+    if userContinues.lower() == "n":
+        break
